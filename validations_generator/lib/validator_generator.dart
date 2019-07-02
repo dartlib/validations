47e1b2d8c86942f0b13e7658500e89206f564e50
Rob Halff <rob.halff@gmail.com>
2019-07-02 19:48:32 +0200

    remove

--- validations_generator/lib/validator_generator.dart
+++ validations_generator/lib/validator_generator.dart
@@ -6,14 +6,10 @@ import 'package:source_gen/source_gen.dart';
 import 'src/generator.dart';
 
 Builder validatorGeneratorFactoryBuilder({String header}) {
-  print('Running validatorGeneratorFactoryBuilder');
-
   return PartBuilder([ValidatorGenerator()], '.gval.dart', header: header);
 }
 
 Builder validatorGeneratorFactory(BuilderOptions options) {
-  print('Running validatorGeneratorFactory');
-
   return validatorGeneratorFactoryBuilder(
       header: options.config['header'] as String);
 }
