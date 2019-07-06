e3f68c4227ab5cd9d8981a60628171625b8d1c1b
Rob Halff <rob.halff@gmail.com>
2019-07-06 02:10:59 +0200

    reset state after public api call

--- validations_generator/test/validator_test.dart
+++ validations_generator/test/validator_test.dart
@@ -3,6 +3,11 @@ import 'package:test/test.dart';
 
 import 'models/car.dart';
 
+printViolations(violations) {
+  violations.forEach((violation) => print(
+      '${violation.message} ${violation.invalidValue} ${violation.validatedObject.runtimeType} ${violation.name} ${violation.propertyPath}'));
+}
+
 void main() {
   group('TestCarValidator', () {
     TestCarValidator validator;
@@ -40,9 +45,8 @@ void main() {
 
         final violations = validator.validate(car);
 
-        // A violation should include more info than this.
-        violations.forEach((violation) => print(
-            '${violation.message} ${violation.invalidValue} ${violation.validatedObject.runtimeType} ${violation.name} ${violation.propertyPath}'));
+        // direct cleanup
+        expect(validator.validationContext.constraintViolations, isEmpty);
 
         expect(
           violations.elementAt(0).message,
@@ -50,34 +54,79 @@ void main() {
         );
 
         expect(
+          violations.elementAt(0).propertyPath,
+          equals('Car.manufacturer'),
+        );
+
+        expect(
           violations.elementAt(1).message,
-          equals('There should be a valid driver!'),
+          equals('Value cannot be null'),
+        );
+
+        expect(
+          violations.elementAt(1).propertyPath,
+          equals('Driver.name'),
         );
 
         expect(
           violations.elementAt(2).message,
-          equals('Value cannot be null'),
+          equals('There should be a valid driver!'),
         );
+
+        expect(
+          violations.elementAt(2).propertyPath,
+          equals('Car.driver'),
+        );
+
         expect(
           violations.elementAt(3).message,
           equals(
               'The license plate D must be between 2 and 14 characters long'),
         );
+
+        expect(
+          violations.elementAt(3).propertyPath,
+          equals('Car.licensePlate'),
+        );
+
         expect(
           violations.elementAt(4).message,
           equals('Car cannot have more than 2 seats'),
         );
+
+        expect(
+          violations.elementAt(4).propertyPath,
+          equals('Car.seatCount'),
+        );
+
         expect(
           violations.elementAt(5).message,
           equals('The top speed 500 is higher than 350'),
         );
+
+        expect(
+          violations.elementAt(5).propertyPath,
+          equals('Car.topSpeed'),
+        );
+
         expect(
           violations.elementAt(6).message,
           equals('Price must not be lower than 100.00'),
         );
+
+        expect(
+          violations.elementAt(6).propertyPath,
+          equals('Car.price'),
+        );
+
         expect(
           violations.elementAt(7).message,
           equals('Car must be registered!'),
+        );
+
+        expect(
+          violations.elementAt(7).propertyPath,
+          equals('Car.isRegistered'),
         );
       });
     });
@@ -86,6 +135,7 @@ void main() {
       test('Validates single properties of correctly configured car.', () {
         final car = Car();
 
+        car.driver = Driver(name: 'TestDriver');
         car.price = Decimal.parse('99.99');
         car.isRegistered = true;
         car.licensePlate = 'DY28-38';
@@ -93,12 +143,15 @@ void main() {
         car.seatCount = 2;
         car.topSpeed = 100;
 
+        expect(validator.validateProperty(car, 'driver'), isEmpty);
         expect(validator.validateProperty(car, 'price'), isEmpty);
         expect(validator.validateProperty(car, 'isRegistered'), isEmpty);
         expect(validator.validateProperty(car, 'licensePlate'), isEmpty);
         expect(validator.validateProperty(car, 'manufacturer'), isEmpty);
         expect(validator.validateProperty(car, 'seatCount'), isEmpty);
         expect(validator.validateProperty(car, 'topSpeed'), isEmpty);
+
+        expect(validator.validationContext.constraintViolations, isEmpty);
       });
 
       test('Collects violations per property.', () {
@@ -106,6 +159,7 @@ void main() {
 
         final car = Car();
 
+        car.driver = Driver();
         car.price = Decimal.parse('100.99');
         car.isRegistered = false;
         car.licensePlate = 'D';
@@ -117,27 +171,34 @@ void main() {
           validator.validateProperty(car, 'manufacturer').first.message,
           equals('Value cannot be null'),
         );
+
         expect(
           validator.validateProperty(car, 'licensePlate').first.message,
           equals(
               'The license plate D must be between 2 and 14 characters long'),
         );
+
         expect(
           validator.validateProperty(car, 'seatCount').first.message,
           equals('Car cannot have more than 2 seats'),
         );
+
         expect(
           validator.validateProperty(car, 'topSpeed').first.message,
           equals('The top speed 500 is higher than 350'),
         );
+
         expect(
           validator.validateProperty(car, 'price').first.message,
           equals('Price must not be lower than 100.00'),
         );
+
         expect(
           validator.validateProperty(car, 'isRegistered').first.message,
           equals('Car must be registered!'),
         );
+
+        expect(validator.validationContext.constraintViolations, isEmpty);
       });
     });
 
@@ -171,6 +232,8 @@ void main() {
         validator.validateValue('isRegistered', false).first.message,
         equals('Car must be registered!'),
       );
+
+      expect(validator.validationContext.constraintViolations, isEmpty);
     });
   });
 }
