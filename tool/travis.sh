3cd8511a32f5c437ec9529658988ffc582d51210
Rob Halff <rob.halff@gmail.com>
2019-07-11 02:24:02 +0200

    fix removal of ensure_build

--- tool/travis.sh
+++ tool/travis.sh
@@ -22,10 +22,6 @@ for PKG in ${PKGS}; do
     echo
     echo -e "\033[1mPKG: ${PKG}; TASK: ${TASK}\033[22m"
     case ${TASK} in
-    command)
-      echo 'pub run build_runner test --delete-conflicting-outputs -- -p chrome'
-      pub run build_runner test --delete-conflicting-outputs -- -p chrome || EXIT_CODE=$?
-      ;;
     dartanalyzer)
       echo 'dartanalyzer --fatal-warnings lib test'
       dartanalyzer --fatal-warnings lib test || EXIT_CODE=$?
@@ -35,12 +31,12 @@ for PKG in ${PKGS}; do
       dartfmt -n --set-exit-if-changed . || EXIT_CODE=$?
       ;;
     test_0)
-      echo 'pub run test'
-      pub run test || EXIT_CODE=$?
+      echo 'pub run test lib/**/*.spec.dart'
+      pub run test lib/**/*.spec.dart || EXIT_CODE=$?
       ;;
     test_1)
-      echo 'pub run test --run-skipped -t presubmit-only test/ensure_build_test.dart'
-      pub run test --run-skipped -t presubmit-only test/ensure_build_test.dart || EXIT_CODE=$?
+      echo 'pub run test'
+      pub run test || EXIT_CODE=$?
       ;;
     *)
       echo -e "\033[31mNot expecting TASK '${TASK}'. Error!\033[0m"
