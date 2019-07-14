2637b49b8849e4c431d15afb509b85c7b702ab2d
Rob Halff <rob.halff@gmail.com>
2019-07-14 16:57:35 +0200

    update example

--- examples/flutter/lib/login_screen.gval.dart
+++ examples/flutter/lib/login_screen.gval.dart
@@ -7,20 +7,29 @@ part of 'login_screen.dart';
 // **************************************************************************
 
 abstract class $_LoginFormValidator implements Validator<FormData> {
+  static String emailNotBlankMessage(Object validatedValue) =>
+      Intl.message('You must fill in an email address',
+          name: 'emailNotBlankMessage', args: [validatedValue]);
+  static String passwordNotBlankMessage(Object validatedValue) =>
+      Intl.message('Password cannot be blank',
+          name: 'passwordNotBlankMessage', args: [validatedValue]);
   static String passwordSizeMessage(int min, int max, Object validatedValue) =>
       Intl.message(
           'Password must be at least be between $min and $max characters long',
           name: 'passwordSizeMessage',
           args: [min, max, validatedValue]);
   @override
-  Map<String, List<ConstraintValidator>> getConstraintValidators() {
-    return {
-      'email': [NotBlankValidator(), EmailValidator()],
-      'password': [
-        NotBlankValidator(),
+  List<FieldValidator> getFieldValidators() {
+    return [
+      FieldValidator<String>(name: 'email', validators: [
+        NotBlankValidator()..message = emailNotBlankMessage,
+        EmailValidator()
+      ]),
+      FieldValidator<String>(name: 'password', validators: [
+        NotBlankValidator()..message = passwordNotBlankMessage,
         SizeValidator(min: 8, max: 12)..message = passwordSizeMessage
-      ]
-    };
+      ])
+    ];
   }
 
   @override
